b7b477ba4f999dc325b20e282f1c2eddc6f676d8
Romashkashtyr <Romainshtyr@yandex.ru>
2025-02-28 15:26:44 +0300

    gradle

--- app/src/main/java/com/sumin/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/sumin/firstcomposeproject/MainActivity.kt
@@ -3,26 +3,69 @@ package com.sumin.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.provider.FontsContractCompat.Columns
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UserInfo(name = "John", age = 25)
+            TimesTable()
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting() {
-    val a = 5
-    val name = "John"
-    Text(text = "Hello $name!")
+fun TimesTable() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        for (i in 1 until 10)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                for (j in 1 until 10) {
+                    val color = if ((i + j) % 2 == 0) {
+                        Color.Yellow
+                    } else {
+                        Color.White
+                    }
+                        Box(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .weight(1f)
+                                .border(width = 1.dp, color = Color.DarkGray)
+                                .background(color = color),
+                            contentAlignment = Alignment.Center
+
+                        ) {
+                            Text(text = "${i * j}")
+                        }
+
+                }
+            }
+    }
 }
 
-@Composable
-fun UserInfo(name: String, age: Int) {
-    Text(text = "Hello $name! You are $age years old")
-}
+
+
+
+
